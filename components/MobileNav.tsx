01714d27686421c70f23539191635da7766fe66b
Vipul2534 <vs7117500@gmail.com>
2025-07-04 21:40:13 +0530

    Bug Fix in MobileNav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -15,6 +15,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import Footer from "./Footer"
+import PlaidLink from "./PlaidLink"
 
 const MobileNav = ({ user }: MobileNavProps) => {
   const pathname = usePathname();
@@ -68,7 +69,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                   </SheetClose>
                 )
               })}
-
+              <PlaidLink user={user} />
               USER
               </nav>
             </SheetClose>
